8d1cf84c249cb3d90b0485fd230b36be74d329ae
Guido Meijer <gtmeijer@gmail.com>
2018-09-03 15:50:44 +0100

    Added readAlf python function to funcGeneral

--- python/plotSessions.py
+++ python/plotSessions.py
@@ -8,9 +8,9 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from funcGeneral import getPaths, pullDrive
+from funcGeneral import getPaths, readAlf
 from os import listdir
-from os.path import join
+from os.path import join, isdir
 
 # Settings
 #subjects = ['6722','6723','6724','6725','6726']
@@ -24,33 +24,34 @@ for i in subjects:
     sessions = [s for s in listdir(join(root_path, i)) if s[0:4] == '2018']
     for j in sessions:
         # Load in data
-        resp = np.load(join(root_path, i, j, '1', 'cwResponse.choice.npy'))
-        contr_left = np.load(join(root_path, i, j, '1', 'cwStimOn.contrastLeft.npy'))
-        contr_right = np.load(join(root_path, i, j, '1', 'cwStimOn.contrastRight.npy'))
-        correct = np.load(join(root_path, i, j, '1', 'cwFeedback.type.npy'))
-        contrasts = np.unique([-contr_left, contr_right])
-        contrasts = contrasts[contrasts != 0]        
+        if isdir(join(root_path, i, j, '2')):
+            ses_dir = '2'
+        else:
+            ses_dir = '1'
         
-        # Correct unequal length vectors
-        if len(correct) == len(contr_left)-1:
-            contr_left = contr_left[0:len(contr_left)-1]
-            contr_right = contr_right[0:len(contr_right)-1]
+        # Read in data to dataframe
+        b_df = readAlf(join(root_path, i, j, ses_dir))
         
+        # Get signed presented contrasts
+        contrast = np.unique([-b_df['contrast_L'], b_df['contrast_R']])
+        contrast = contrast[contrast != 0]
+                
         # Get psychometric curves
-        for c in contrasts:
-            #psy_curve.loc[0,c] = np.sum(correct[(-contr_left == c) | (contr_right == c)] == 1) / len(correct[(-contr_left == c) | (contr_right == c)])
-            psy_curve.loc[c,j[8:10]+'/'+j[5:7]] = np.sum(resp[(-contr_left == c) | (contr_right == c)] == 2) / len(resp[(-contr_left == c) | (contr_right == c)] == 2)
+        for c in contrast:
+            psy_curve.loc[c,j[8:10]+'/'+j[5:7]] = np.sum(b_df['choice'][(-b_df['contrast_L'] == c) | (b_df['contrast_R'] == c)] == 2) / len(b_df['choice'][(-b_df['contrast_L'] == c) | (b_df['contrast_R'] == c)] == 2)
         
         # Get performance
-        perf_df.loc['correct',j[8:10]+'/'+j[5:7]] = np.sum(correct == 1) / len(correct)
-        perf_df.loc['num_trials',j[8:10]+'/'+j[5:7]] = len(correct)
-        perf_df.loc['bias',j[8:10]+'/'+j[5:7]] = (np.sum(resp == 2) - np.sum(resp == 1)) / (np.sum(resp == 2) + np.sum(resp == 1))
+        perf_df.loc['correct',j[8:10]+'/'+j[5:7]] = np.sum(b_df['correct'] == 1) / len(b_df['correct'])
+        perf_df.loc['num_trials',j[8:10]+'/'+j[5:7]] = len(b_df['correct'])
+        perf_df.loc['bias',j[8:10]+'/'+j[5:7]] = (np.sum(b_df['choice'] == 2) - np.sum(b_df['choice'] == 1)) / (np.sum(b_df['choice'] == 2) + np.sum(b_df['choice'] == 1))
         
     f, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)    
     sns.lineplot(data=psy_curve, ax=ax1, palette="YlGnBu_d", dashes=False, marker="o")
     ax1.plot([-1, 1], [0.5, 0.5], '--k')
     ax1.set_ylabel('P(right)')
     ax1.set_xlabel('Signed contrast')
+    ax1.set_title(i)
+    ax1.legend().set_visible(False)
     
     ax2.plot(perf_df.loc['correct'], '-ok')
     ax2.set_ylabel('Percentage correct')    
@@ -62,9 +63,12 @@ for i in subjects:
     plt.tight_layout()
     figManager = plt.get_current_fig_manager()
     figManager.window.showMaximized()
-    plt.savefig(plot_path + i, dpi=300)
-    plt.show()
     
+    fig = plt.gcf()
+    fig.set_size_inches((10, 8), forward=False)
+    fig.savefig(plot_path + i, dpi=300)
+    
+    plt.show()
 
         
         
