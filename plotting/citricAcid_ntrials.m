0c55fd323681305aecde35a2967daa38164a8284
Anne Urai <anne.urai@gmail.com>
2018-10-13 10:23:01 -0400

    better citric acid plots

--- plotting/citricAcid_ntrials.m
+++ plotting/citricAcid_ntrials.m
@@ -7,12 +7,11 @@ data = readAlf_allData(fullfile(getenv('HOME'), 'Google Drive', 'IBL_DATA_SHARE'
 
 foldername = fullfile(getenv('HOME'), 'Google Drive', 'Rig building WG', ...
     'DataFigures', 'BehaviourData_Weekly', '2018-10-09');
-set(groot, 'defaultaxesfontsize', 7, 'DefaultFigureWindowStyle', 'normal');
 
 % grab only those dates that are immediately before and after citric acid
 % or normal water intervention
 
-data = data(ismember(datenum(data.date), datenum({'2018-09-17', '2018-09-24', '2018-10-01', '2018-10-09'})), :);
+data = data(ismember(datenum(data.date), datenum({'2018-09-24', '2018-10-01', '2018-10-09'})), :);
 [~, data.weekday] = weekday(datenum(data.date));
 data.weekday = cellstr(data.weekday);
 
@@ -26,46 +25,50 @@ data_tmp = data(:, {'trialNum', 'animal', 'date'});
 data_mat = unstack(data_tmp, {'trialNum'}, 'date', 'AggregationFunction', @max);
 
 % remove two mice that already got CA hydrogel a week early
-data_mat{ismember(data_mat.animal, {'IBL_10', 'IBL_8'}), 3} = NaN;
+data_mat{ismember(data_mat.animal, {'IBL_10', 'IBL_8'}), 2} = NaN;
 
 % separate out the mice that got sucrose on the 9th of October in the rig
-sucrosetrials = data_mat{ismember(data_mat.animal, {'IBL_33', 'IBL_13'}), 5};
+sucrosetrials = data_mat{ismember(data_mat.animal, {'IBL_33', 'IBL_13'}), 4};
 data_mat{ismember(data_mat.animal, {'IBL_33', 'IBL_13'}), 5} = NaN;
 
 data_mat = data_mat{:, 2:end};
 xvars = repmat([1 2 3 4], [size(data_mat, 1) 1]);
 
 %% BARGRAPH WITH SCATTER
+set(groot, 'defaultaxesfontsize', 7, 'DefaultFigureWindowStyle', 'normal');
 
 close all; 
-subplot(3,3,[1 2]); hold on;
+subplot(3,3,[1]); hold on;
 s = scatter(xvars(:), data_mat(:), 10,  [0.5 0.5 0.5], 'o', 'jitter', 'on');
-scatter([4 4], sucrosetrials, 15, [0.5 0.5 0.5], 'd', 'jitter', 'on');
+scatter([3 3], sucrosetrials, 15, [0.5 0.5 0.5], 'd', 'jitter', 'on');
 
 % different errorbars
 colors = linspecer(4);
-e1 = errorbar(1:2, nanmean(data_mat(:, 1:2)), nanstd(data_mat(:, 1:2)), 'o', 'color',  colors(1, :), 'markerfacecolor', 'w', 'markeredgecolor', colors(1, :), 'linewidth', 1, 'capsize', 0);
-e2 = errorbar(3, nanmean(data_mat(:, 3)), nanstd(data_mat(:, 3)), 'o', 'color',  colors(2, :), 'markerfacecolor', 'w', 'markeredgecolor', colors(2, :), 'linewidth', 1, 'capsize', 0);
-e3 = errorbar(4, nanmean(data_mat(:, 4)), nanstd(data_mat(:, 4)), 'o', 'color',  colors(3, :), 'markerfacecolor', 'w', 'markeredgecolor', colors(3, :), 'linewidth', 1, 'capsize', 0);
+for i = 1:4,
+    e{i} = errorbar(i, nanmean(data_mat(:, i)), nanstd(data_mat(:, i)), ...
+        'o', 'color',  colors(i, :), 'markerfacecolor', 'w', 'markeredgecolor', colors(i, :), 'linewidth', 1, 'capsize', 0);
+end
+xlim([0.5 3.5]);
 
-xlim([0.5 4.5]);
-set(gca, 'xtick', 1:4, 'xticklabel', datestr(unique(data.date), 1), 'xticklabelrotation', -20);
-ylabel('Number of trials on Monday');
+
+set(gca, 'xtick', 1:3, 'xticklabel', unique(data.water, 'stable'), 'xticklabelrotation', -20);
+title('Water restriction regimes');
+
+ylabel({'Number of trials' 'on Monday (CSHL)'});
 offsetAxes;
-legend([e1 e2 e3], unique(data.water, 'stable'), 'location', 'eastoutside', 'box', 'off');
+%subplot(333); axis off;
+% 
+% % add the pairs
+% subplot(444);
+% scatter(data_mat(:, 2), data_mat(:, 4), 15, [0.5 0.5 0.5], 'o');
+% xlabel('Trials after 1ml/day'); ylabel('Trials after 2% CA water');
+% axis square;
+% axisEqual; r = refline(1,0); r.Color = 'k'; r.LineWidth = 0.5;
+% %offsetAxes;
 
-subplot(333); axis off;
-
-% add the pairs
-subplot(444);
-scatter(data_mat(:, 2), data_mat(:, 4), 15, [0.5 0.5 0.5], 'o');
-xlabel('Trials after 1ml/day'); ylabel('Trials after 2% CA water');
-axis square;
-axisEqual; r = refline(1,0); r.Color = 'k'; r.LineWidth = 0.5;
-%offsetAxes;
-
-%tightfig;
+tightfig;
 print(gcf, '-dpdf', fullfile(foldername, 'citricAcid_trialCounts_CSHL.pdf'));
+print(gcf, '-dpdf', '/Users/urai/Google Drive/2018 Postdoc CSHL/CitricAcid/citricAcid_trialCounts_CSHL.pdf');
 
 % 
 % %% pivot the table
