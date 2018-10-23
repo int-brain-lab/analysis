31dedae392a6598581aa888c7350c2b9ec33cedd
Anne Urai <anne.urai@gmail.com>
2018-10-23 10:43:01 -0400

    pull data from ONE instead of Google Drive

---
+++ helper/dataset2table.m
@@ -0,0 +1,26 @@
+function outp = dataset2table(D, ses)
+
+% convert ONE dataset into an easy-to-work-with behavioral table
+varnames    = regexprep(D.dataset_type, '_ibl_trials.', '');
+s           = cell2struct(D.data, varnames);
+
+% add some useful things for behavioral analysis
+s.signedContrast = (s.contrastLeft - s.contrastRight) * 100;
+s.rt             = s.response_times - s.stimOn_times;
+s.correct        = double(sign(s.signedContrast) == s.choice);
+s.contrastRight(s.signedContrast == 0) = NaN;
+
+% metadata from 'ses'
+sesflds = {'subject', 'location', 'lab', 'start_time', 'end_time'};
+for sidx = 1:length(sesflds),
+    s.(sesflds{sidx}) = repmat(ses.(sesflds{sidx}) , length(s.choice), 1);
+end
+
+% if possible, convert
+if ~verLessThan('matlab','8.2')
+    outp = struct2table(s);
+else
+    outp = s;
+end
+
+end
