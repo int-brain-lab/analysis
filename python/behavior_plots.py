724ec035a9e8238c25bc79450df09491cea7e409
Anne Urai <anne.urai@gmail.com>
2018-12-10 20:44:59 -0500

    moved plotting functions from first column of snapshot to separate behavior_plots.py

--- python/behavior_plots.py
+++ python/behavior_plots.py
@@ -13,6 +13,8 @@ import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+from datetime import timedelta
+
 #from matplotlib.dates import MONDAY
 import psychofit as psy # https://github.com/cortex-lab/psychofit
 import seaborn as sns 
@@ -89,6 +91,134 @@ def plot_chronometric(df, ax, color):
     ax.set_xticks([-100, -50, 0, 50, 100])
     ax.set_xticklabels(['-100', '-50', '0', '50', '100'])
 
+def plot_water_weight_curve(weight_water, baseline, ax):
+
+    xlims = [weight_water.date.min()-timedelta(days=2), weight_water.date.max()+timedelta(days=2)]
+
+    # use pandas plot for a stacked bar - water types
+    wa_unstacked = weight_water.pivot_table(index='days',
+        columns='water_type', values='water_administered', aggfunc='sum').reset_index()
+
+    # shorten names for legend
+    wa_unstacked.columns = wa_unstacked.columns.str.replace("Water", "Wa")
+    wa_unstacked.columns = wa_unstacked.columns.str.replace("Sucrose", "Sucr")
+    wa_unstacked.columns = wa_unstacked.columns.str.replace("Citric Acid", "CA")
+    wa_unstacked.columns = wa_unstacked.columns.str.replace("Hydrogel", "Hdrg")
+
+    # mark the citric acid columns to indicate adlib amount
+    for ic, c in enumerate(wa_unstacked.columns):
+        if 'CA' in c:
+            wa_unstacked[c].replace({0:2}, inplace=True)
+
+    # https://stackoverflow.com/questions/44250445/pandas-bar-plot-with-continuous-x-axis
+    plotvar       = wa_unstacked
+    plotvar.index = plotvar.days
+    plotvar.drop(columns='days', inplace=True)
+    plotvar = plotvar.reindex(np.arange(weight_water.days.min()-2, weight_water.days.max()+2))
+
+    # sort the columns by possible water types
+    plotvar = plotvar[sorted(list(plotvar.columns.values), reverse=True)]
+    plotvar.plot(kind='bar', style='.', stacked=True, ax=ax, edgecolor="none")
+    l = ax.legend(loc='lower left', prop={'size': 'xx-small'},
+        bbox_to_anchor=(0., 1.02, 1., .102),
+        ncol=2, mode="expand", borderaxespad=0., frameon=False)
+    l.set_title('')
+    ax.set(ylabel="Water intake (mL)", xlabel='', xlim=xlims)
+    ax.yaxis.label.set_color("#0072B2")
+
+    # overlay the weight curve
+    weight_water2 = weight_water.groupby('days').mean().reset_index()
+    weight_water2 = weight_water2.dropna(subset=['weight'])
+    righty = ax.twinx()
+    sns.lineplot(x=weight_water2.days, y=weight_water2.weight, ax=righty, color='.15', marker='o')
+    righty.set(xlabel='', ylabel="Weight (g)",
+        xlim=[weight_water.days.min()-2, weight_water.days.max()+2], ylim=[baseline*0.8, baseline*1.2])
+    righty.grid(False)
+
+    # correct the ticks to show dates, not days
+    # also indicate Mondays by grid lines
+    ax.set_xticks([weight_water.days[i] for i, dt in enumerate(weight_water.date) if dt.weekday() is 0])
+    ax.set_xticklabels([weight_water.date[i].strftime('%b-%d') for i, dt in enumerate(weight_water.date) if dt.weekday() is 0])
+    for item in ax.get_xticklabels():
+        item.set_rotation(60)
+
+def plot_trialcounts_sessionlength(behav, ax, xlims):
+
+    trialcounts = behav.groupby(['date'])['trial'].max().reset_index()
+    sns.lineplot(x="date", y="trial", marker='o', color=".15", data=trialcounts, ax=ax)
+    ax.set(xlabel='', ylabel="Trial count", xlim=xlims)
+
+    # compute the length of each session
+    behav['sessionlength'] = (behav.end_time - behav.start_time)
+    behav['sessionlength'] = behav.sessionlength.dt.total_seconds() / 60
+    sessionlength = behav.groupby(['date'])['sessionlength'].mean().reset_index()
+
+    righty = ax.twinx()
+    sns.lineplot(x="date", y="sessionlength", marker='o', color="firebrick", data=sessionlength, ax=righty)
+    righty.yaxis.label.set_color("firebrick")
+    righty.tick_params(axis='y', colors='firebrick')
+    righty.set(xlabel='', ylabel="Session (min)", ylim=[0,80], xlim=xlims)
+
+    righty.grid(False)
+    fix_date_axis(righty)
+    fix_date_axis(ax)
+
+def plot_performance_rt(behav, ax, xlims):
+
+    behav['correct_easy'] = behav.correct
+    behav.loc[np.abs(behav['signedContrast']) < 50, 'correct_easy'] = np.NaN
+    correct_easy = behav.groupby(['date'])['correct_easy'].mean().reset_index()
+
+    sns.lineplot(x="date", y="correct_easy", marker='o', color=".15", data=correct_easy, ax=ax)
+    ax.set(xlabel='', ylabel="Performance (easy trials)",
+        xlim=xlims, yticks=[0.5, 0.75, 1], ylim=[0.4, 1.01])
+    # ax.yaxis.label.set_color("black")
+
+    # RTs on right y-axis
+    trialcounts = behav.groupby(['date'])['rt'].median().reset_index()
+    righty = ax.twinx()
+    sns.lineplot(x="date", y="rt", marker='o', color="firebrick", data=trialcounts, ax=righty)
+
+    righty.yaxis.label.set_color("firebrick")
+    righty.tick_params(axis='y', colors='firebrick')
+    righty.set(xlabel='', ylabel="RT (s)", ylim=[0.1,10], xlim=xlims)
+    righty.set_yscale("log")
+
+    righty.yaxis.set_major_formatter(mpl.ticker.FuncFormatter(lambda y,pos: ('{{:.{:1d}f}}'.format(int(np.maximum(-np.log10(y),0)))).format(y)))
+    righty.grid(False)
+    fix_date_axis(righty)
+    fix_date_axis(ax)
+
+def plot_contrast_heatmap(behav, ax):
+
+    import copy; cmap=copy.copy(plt.get_cmap('vlag'))
+    cmap.set_bad(color="w") # remove those squares
+
+    # TODO: only take the mean when there is more than 1 trial (to remove bug in early sessions)
+    pp  = behav.groupby(['signedContrast', 'days']).agg({'choice2':'mean'}).reset_index()
+    pp2 = pp.pivot("signedContrast", "days",  "choice2").sort_values(by='signedContrast', ascending=False)
+    pp2 = pp2.reindex([-100, -50, -25, -12, -6, 0, 6, 12, 25, 50, 100])
+
+    # inset axes for colorbar, to the right of plot
+    axins1 = inset_axes(ax, width="5%", height="90%", loc='right',
+    bbox_to_anchor=(0.15, 0., 1, 1), bbox_transform=ax.transAxes, borderpad=0,)
+    # now heatmap
+    sns.heatmap(pp2, linewidths=.5, ax=ax, vmin=0, vmax=1, cmap=cmap, cbar=True,
+    cbar_ax=axins1,
+    cbar_kws={'label': 'Choose right (%)', 'shrink': 0.8, 'ticks': []})
+    ax.set(ylabel="Contrast (%)")
+
+    # fix the date axis
+    dates  = behav.date.unique()
+    xpos   = np.arange(len(dates)) + 0.5 # the tick locations for each day
+    xticks = [i for i, dt in enumerate(dates) if pd.to_datetime(dt).weekday() is 0]
+    ax.set_xticks(np.array(xticks) + 0.5)
+
+    xticklabels = [pd.to_datetime(dt).strftime('%b-%d') for i, dt in enumerate(dates) if pd.to_datetime(dt).weekday() is 0]
+    ax.set_xticklabels(xticklabels)
+    for item in ax.get_xticklabels():
+        item.set_rotation(60)
+    ax.set(xlabel='')
 
 def fix_date_axis(ax):
     # deal with date axis and make nice looking 
--- python/behavioral_overview_perlab.py
+++ python/behavioral_overview_perlab.py
@@ -43,87 +43,51 @@ users = ['miles', 'ines', 'valeria']
 
 for lidx, lab in enumerate(users):
 
-	subjects 		= pd.DataFrame(one.alyx.get('/subjects?water_restricted=True&alive=True&stock=False&responsible_user=%s'%lab))
-	fig1, axes1 	= plt.subplots(ncols=int(np.ceil(np.sqrt(len(subjects)))), nrows=int(np.ceil(np.sqrt(len(subjects)))), 
-		constrained_layout=True, figsize=(11.69, 8.27))
-	axes1 = axes1.flatten() # to enable 1d indexing
+	subjects = pd.DataFrame(one.alyx.get('/subjects?water_restricted=True&alive=True&stock=False&responsible_user=%s'%lab))
 
-	fig2, axes2 	= plt.subplots(ncols=int(np.ceil(np.sqrt(len(subjects)))), nrows=int(np.ceil(np.sqrt(len(subjects)))), 
-		constrained_layout=True, figsize=(11.69, 8.27))
-	axes2 = axes2.flatten() # to enable 1d indexing
+	# group by batches: mice that were born on the same day
+	batches = subjects.birth_date.unique()
 
-	for i, mouse in enumerate(subjects['nickname']):
+	for birth_date in batches:
 
-		print(mouse)
-		try:
+		mice = subjects.loc[subjects['birth_date'] == '2018-07-17']['nickname']
+		print(mice)
+		fig, axes = plt.subplots(ncols=len(mice), nrows=4, constrained_layout=False, figsize=(11.69, 8.27))
+		sns.set_palette("colorblind") # palette for water types
+		fig.suptitle('Mice born on %s, user %s' %(birth_date, lab))
+
+		for i, mouse in enumerate(mice):
+
+			# ============================================= #
+			# WEIGHT CURVE AND WATER INTAKE
+			# ============================================= #
+
+			# get all the weights and water aligned in 1 table
+			weight_water, baseline = get_water_weight(mouse)
+
+			# determine x limits
+			xlims = [weight_water.date.min()-timedelta(days=2), weight_water.date.max()+timedelta(days=2)]
+			plot_water_weight_curve(weight_water, baseline, axes[0,i])
+			axes[0,i].set_title(mouse)
+
+			# ============================================= #
+			# TRIAL COUNTS AND SESSION DURATION
+			# ============================================= #
+
 			behav 	= get_behavior(mouse)
+			plot_trialcounts_sessionlength(behav, axes[1,i], xlims)
 
 			# ============================================= #
 			# PERFORMANCE AND MEDIAN RT
 			# ============================================= #
 
-			# performance on easy trials
-			ax = axes1[i]
-			behav['correct_easy'] = behav.correct
-			behav.loc[np.abs(behav['signedContrast']) < 50, 'correct_easy'] = np.NaN
-			correct_easy = behav.groupby(['date'])['correct_easy'].mean().reset_index()
-
-			sns.lineplot(x="date", y="correct_easy", marker='o', color=".15", data=correct_easy, ax=ax)
-			ax.set(xlabel='', ylabel="Performance (easy trials)",
-				yticks=[0.5, 0.75, 1], ylim=[0.4, 1.01], title=mouse)
-			# ax.yaxis.label.set_color("black")
-
-			# RTs on right y-axis
-			trialcounts = behav.groupby(['date'])['rt'].median().reset_index()
-			righty = ax.twinx()
-			sns.lineplot(x="date", y="rt", marker='o', color="firebrick", data=trialcounts, ax=righty)
-
-			righty.yaxis.label.set_color("firebrick")
-			righty.tick_params(axis='y', colors='firebrick')
-			righty.set(xlabel='', ylabel="RT (s)", ylim=[0.1,10])
-			righty.set_yscale("log")
-
-			righty.yaxis.set_major_formatter(mpl.ticker.FuncFormatter(lambda y,pos: ('{{:.{:1d}f}}'.format(int(np.maximum(-np.log10(y),0)))).format(y)))
-			righty.grid(False)
-			fix_date_axis(righty)
-			fix_date_axis(ax)
+			plot_performance_rt(behav, axes[2,i], xlims)
 
 			# ============================================= #
 			# CONTRAST/CHOICE HEATMAP
 			# ============================================= #
 
-			ax = axes2[i]
-			import copy; cmap=copy.copy(plt.get_cmap('vlag'))
-			cmap.set_bad(color="w") # remove those squares
+			plot_contrast_heatmap(behav, axes[3,i])
 
-	        # TODO: only take the mean when there is more than 1 trial (to remove bug in early sessions)
-			pp  = behav.groupby(['signedContrast', 'days']).agg({'choice2':'mean'}).reset_index()
-			pp2 = pp.pivot("signedContrast", "days",  "choice2").sort_values(by='signedContrast', ascending=False)
-			pp2 = pp2.reindex([-100, -50, -25, -12, -6, 0, 6, 12, 25, 50, 100])
-
-			# inset axes for colorbar, to the right of plot
-			axins1 = inset_axes(ax, width="5%", height="90%", loc='right',
-				bbox_to_anchor=(0.15, 0., 1, 1), bbox_transform=ax.transAxes, borderpad=0,)
-			# now heatmap
-			sns.heatmap(pp2, linewidths=.5, ax=ax, vmin=0, vmax=1, cmap=cmap, cbar=True,
-				cbar_kws={'label': 'Choose right (%)', 'shrink': 0.8, 'ticks': []})
-			ax.set(ylabel="Contrast (%)", title=mouse)
-
-			# fix the date axis
-			dates  = behav.date.unique()
-			xpos   = np.arange(len(dates)) + 0.5 # the tick locations for each day
-			xticks = [i for i, dt in enumerate(dates) if pd.to_datetime(dt).weekday() is 0]
-			ax.set_xticks(np.array(xticks) + 0.5)
-
-			xticklabels = [pd.to_datetime(dt).strftime('%b-%d') for i, dt in enumerate(dates) if pd.to_datetime(dt).weekday() is 0]
-			ax.set_xticklabels(xticklabels)
-			for item in ax.get_xticklabels():
-				item.set_rotation(60)
-			ax.set(xlabel='')
-		except:
-			pass
-
-	print("%s failed to run" %mouse)
-	fig1.savefig(join(path + '%s_overview_perf_rt.pdf'%lab))
-	fig2.savefig(join(path + '%s_overview_contrast_heatmap.pdf'%lab))
+		fig.savefig(join(path + '%s_overview_batch_%s.pdf'%(lab, birth_date)))
 
