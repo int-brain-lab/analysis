9c923b9064356912931a3cc585f4070eea595545
Anne Urai <anne.urai@gmail.com>
2018-12-11 18:35:16 -0500

    plot per lab now works

--- python/behavioral_overview_perlab.py
+++ python/behavioral_overview_perlab.py
@@ -14,14 +14,11 @@ from IPython import embed as shell
 
 # IBL stuff
 from oneibl.one import ONE
-import psychofit as psy # https://github.com/cortex-lab/psychofit
 
 # loading and plotting functions
 from define_paths import fig_path
-from os.path import join as join
 from behavior_plots import *
 from load_mouse_data import * # this has all plotting functions
-
 
 ## INITIALIZE A FEW THINGS
 sns.set_style("darkgrid", {'xtick.bottom': True,'ytick.left': True, 'lines.markeredgewidth':0 } )
@@ -35,7 +32,7 @@ path = fig_path()
 if not os.path.exists(path):
     os.mkdir(path)
 
-users = ['miles', 'valeria', 'ines']
+users = ['ines', 'valeria', 'miles']
 
 # ============================================= #
 # START BIG OVERVIEW PLOT
@@ -43,7 +40,7 @@ users = ['miles', 'valeria', 'ines']
 
 for lidx, lab in enumerate(users):
 
-	subjects = pd.DataFrame(one.alyx.get('/subjects?water_restricted=True&alive=True&stock=False&responsible_user=%s'%lab))
+	subjects = pd.DataFrame(one.alyx.get('/subjects?water_restricted=True&alive=True&responsible_user=%s'%lab))
 
 	# group by batches: mice that were born on the same day
 	batches = subjects.birth_date.unique()
@@ -54,20 +51,20 @@ for lidx, lab in enumerate(users):
 		print(mice)
 		fig, axes = plt.subplots(ncols=max([len(mice), 4]), nrows=4, constrained_layout=False, figsize=(11.69, 8.27))
 		sns.set_palette("colorblind") # palette for water types
-		
 
 		for i, mouse in enumerate(mice):
 			print(mouse)
 
 			try:
+
 				# WEIGHT CURVE AND WATER INTAKE
+				t = time.time()
 				weight_water, baseline = get_water_weight(mouse)
 
 				# determine x limits
 				xlims = [weight_water.date.min()-timedelta(days=2), weight_water.date.max()+timedelta(days=2)]
 				plot_water_weight_curve(weight_water, baseline, axes[0,i])
-				axes[0,i].set_title(mouse, fontweight="bold")
-
+				
 				# TRIAL COUNTS AND SESSION DURATION
 				behav 	= get_behavior(mouse)
 				plot_trialcounts_sessionlength(behav, axes[1,i], xlims)
@@ -77,12 +74,17 @@ for lidx, lab in enumerate(users):
 
 				# CONTRAST/CHOICE HEATMAP
 				plot_contrast_heatmap(behav, axes[3,i])
+				axes[3,i].set_xlabel("Mouse " + mouse, fontweight="bold")
+
+				elapsed = time.time() - t
+				print( "Elapsed time: %f seconds.\n" %elapsed )
+
 			except:
 				pass
 
 		# SAVE PER BATCH, MAX 5
 		fig.suptitle('Mice born on %s, user %s' %(birth_date, lab))
 		plt.tight_layout(rect=[0, 0.03, 1, 0.95])
-		fig.savefig(join(path + '%s_overview_batch_%s.pdf'%(lab, birth_date)))
+		fig.savefig(os.path.join(path + '%s_overview_batch_%s.pdf'%(lab, birth_date)))
 		plt.close(fig)
 
