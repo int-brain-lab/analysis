170b6aafb1b02e4185769d4003677bc97a51e393
Anne Urai <anne.urai@gmail.com>
2019-06-03 22:59:05 +0200

    added contrast set comparisons for ines

--- python/contrast_sets.py
+++ python/contrast_sets.py
@@ -27,16 +27,22 @@ figpath  = os.path.join(os.path.expanduser('~'), 'Data/Figures_IBL')
 # COMPARE DIFFERENT CONTRAST SETS
 # ================================================================== #
 
-labs = ['churchlandlab']
+labs = ['churchlandlab', 'mainenlab']
 # hack to get around SQL limit
 for lidx, lab in enumerate(labs):
 
 	print(lab)
-	sess = (acquisition.Session)
-	# old mice, latest sessions (to check different contrast sets)
-	b = (behavior.TrialSet.Trial & (subject.SubjectLab() & 'lab_name="%s"'%lab)) \
-		* (acquisition.Session() & 'session_start_time > "2019-03-01"'  & 'task_protocol LIKE "%biased%"') \
-		* (subject.Subject() & 'subject_birth_date < "2018-08-01"') * subject.SubjectLab.proj('lab_name')
+
+	if 'churchlandlab' in lab:
+
+		# old mice, latest sessions (to check different contrast sets)
+		b = (behavior.TrialSet.Trial & (subject.SubjectLab() & 'lab_name="%s"'%lab)) \
+			* (acquisition.Session() & 'session_start_time > "2019-03-01"'  & 'task_protocol LIKE "%biased%"') \
+			* (subject.Subject() & 'subject_birth_date < "2018-08-01"') * subject.SubjectLab.proj('lab_name')
+	elif 'mainenlab' in lab:
+		b = (behavior.TrialSet.Trial & (subject.SubjectLab() & 'lab_name="%s"'%lab)) \
+			* (acquisition.Session() & 'session_start_time > "2019-04-01"'  & 'task_protocol LIKE "%biased%"') \
+			* (subject.Subject() & 'subject_nickname in ("ZM_1369", "ZM_1371", "ZM_1372")') * subject.SubjectLab.proj('lab_name')
 
 	bdat = pd.DataFrame(b.fetch(order_by='subject_nickname, session_start_time, trial_id'))
 	print(bdat['subject_nickname'].unique())
@@ -49,15 +55,20 @@ for lidx, lab in enumerate(labs):
 # recode
 behav = dj2pandas(behav)
 
+# CODE FOR WHETHER A LOW CONTRAST IS PRESENT OR NOT
+behav['trial_stim_contrast_right'] = behav['trial_stim_contrast_right'] * 100
+behav['trial_stim_contrast_right'] = behav.trial_stim_contrast_right.astype(int)
+behav['trial_stim_contrast_right'] = behav['trial_stim_contrast_right'].replace(0, 100)
+
 # indicate the contrast set for each session
 behav['lower_contrasts'] = behav.groupby(['subject_nickname', 'session_start_time', 
-	'lab_name'])['signed_contrast'].transform(lambda x: 2 in x.unique())
+	'lab_name'])['trial_stim_contrast_right'].transform(lambda x: np.any(x < 5))
 behav.groupby(['subject_nickname', 'session_start_time'])['lower_contrasts'].unique()
 
-# SEPARATELY FOR INIT_UNBIASED
+# PLOT SEPARATELY PER CONTRAST SET
 cmap = sns.diverging_palette(20, 220, n=len(behav['probabilityLeft'].unique()), center="dark")
 fig = sns.FacetGrid(behav, hue="probabilityLeft", row="subject_nickname", col="lower_contrasts", palette=cmap)
-fig.map(plot_psychometric, "signed_contrast", "choice_right").add_legend()
+fig.map(plot_psychometric, "signed_contrast", "choice_right", "subject_nickname").add_legend()
 fig.despine(trim=True)
 fig.savefig(os.path.join(figpath, "contrast_sets.pdf"))
 print('done')
@@ -101,11 +112,12 @@ biasshift = biasshift.reset_index()
 biasshift = biasshift.rename(columns={0: 'bias_shift'})
 
 # make an overview plot of all bias shifts, one line for each subject
-plt.close("all")
+plt.close("all")	
 fig, ax = plt.subplots()
-sns.pointplot(x="lower_contrasts", y="bias_shift", hue='subject_nickname', 
+sns.pointplot(x="lower_contrasts", y="bias_shift", hue='subject_nickname',
 	markers='None', color="0.9", join=True, data=biasshift, legend=False)
-ax = sns.pointplot(x="lower_contrasts", y="bias_shift", dodge=True, join=True, data=biasshift, legend=False)
+ax = sns.pointplot(x="lower_contrasts", y="bias_shift", hue='lab_name', 
+	dodge=True, join=True, data=biasshift, legend=False)
 ax.set_ylabel('Bias shift (% choice)')
 plt.setp(ax.lines, zorder=1000)
 # improve the legend
