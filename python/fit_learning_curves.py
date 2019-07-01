351a329fbbaa4c45bf6b72994948677be376c8be
Anne Urai <anne.urai@gmail.com>
2019-07-01 16:51:53 -0400

    corrected learning curve fit, delay is now difference between 80 and 20% performance

--- python/fit_learning_curves.py
+++ python/fit_learning_curves.py
@@ -32,10 +32,10 @@ def fit_learningcurve(df):
         # get some more parameters out
         fit_x = sigmoid(vec_x, par[0], par[1], par[2])
 
-        norm_x = (fit_x - min(fit_x)) / max(fit_x - min(fit_x)) # why this normalization step?
-        delay = vec_x[np.argmin(np.abs(norm_x - 0.2))]
-        rise = vec_x[np.argmin(np.abs(norm_x - 0.8))]
-        asymp = vec_x[np.argmin(np.abs(norm_x - 0.99))]
+        asymp  = max(fit_x) # asymptotic performance for this animal
+        norm_x = (fit_x - min(fit_x)) / max(fit_x - min(fit_x)) # normalize learning to the asymptote
+        delay  = vec_x[np.argmin(np.abs(norm_x - 0.2))] # how long to reach 20% of performance?
+        rise   = vec_x[np.argmin(np.abs(norm_x - 0.8))] - delay # after delay, how long to reach 80% of performance?
 
         df2 = pd.DataFrame(
             {'delay': delay, 'rise': rise, 'asymp': asymp, 'alpha': par[0], 'beta': par[1], 'gamma': par[2],
