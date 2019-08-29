c19c8e22264a0e3bcf3ab0bc733d30effcb1a4fc
Guido Meijer <gtmeijer@gmail.com>
2019-08-29 11:55:18 +0100

    Made code PEP8 compliant

--- paper-behavior/figure6_decoding_lab_membership.py
+++ paper-behavior/figure6_decoding_lab_membership.py
@@ -28,10 +28,10 @@ from sklearn.metrics import f1_score
 path = '/home/guido/Figures/Behavior/'
 iterations = 2000     # how often to decode
 num_splits = 3        # n in n-fold cross validation
-#decoding_metrics = ['perf_easy','n_trials','threshold','bias','reaction_time','training_time']
-#decoding_metrics_control = ['perf_easy','n_trials','threshold','bias','reaction_time','training_time','time_zone']
-decoding_metrics = ['perf_easy','n_trials','threshold','bias','reaction_time']
-decoding_metrics_control = ['perf_easy','n_trials','threshold','bias','reaction_time','time_zone']
+decoding_metrics = ['perf_easy', 'n_trials', 'threshold', 'bias', 'reaction_time']
+decoding_metrics_control = ['perf_easy', 'n_trials', 'threshold', 'bias', 'reaction_time',
+                            'time_zone']
+
 
 # Decoding function with n-fold cross validation
 def decoding(resp, labels, clf, num_splits):
@@ -47,64 +47,91 @@ def decoding(resp, labels, clf, num_splits):
     f1 = f1_score(y_true, y_pred, labels=np.unique(labels), average='micro')
     return f1
 
+
 # Query list of subjects
-use_subjects = subject.Subject * subject.SubjectLab * subject.SubjectProject & 'subject_project = "ibl_neuropixel_brainwide_01"'
+use_subjects = (subject.Subject * subject.SubjectLab * subject.SubjectProject
+                & 'subject_project = "ibl_neuropixel_brainwide_01"')
 subjects = use_subjects.fetch('subject_nickname')
 
-# Create dataframe with behavioral metrics of all mice        
-learning = pd.DataFrame(columns=['mouse','lab','time_zone','learned','date_learned','training_time','perf_easy','n_trials','threshold','bias','reaction_time','lapse_low','lapse_high'])
+# Create dataframe with behavioral metrics of all mice
+learning = pd.DataFrame(columns=['mouse', 'lab', 'time_zone', 'learned', 'date_learned',
+                                 'training_time', 'perf_easy', 'n_trials', 'threshold',
+                                 'bias', 'reaction_time', 'lapse_low', 'lapse_high'])
 for i, nickname in enumerate(subjects):
-    if np.mod(i+1,10) == 0: 
-        print('Loading data of subject %d of %d'%(i+1,len(subjects)))
-    
+    if np.mod(i+1, 10) == 0:
+        print('Loading data of subject %d of %d' % (i+1, len(subjects)))
+
     # Gather behavioral data for subject
-    subj = subject.Subject * subject.SubjectLab & 'subject_nickname="%s"'%nickname
-    training = pd.DataFrame(behavior_analysis.SessionTrainingStatus * subject.Subject & 'subject_nickname="%s"'%nickname)
-    behav = pd.DataFrame((behavior_analysis.BehavioralSummaryByDate * subject.Subject * subject.SubjectLab &
-       'subject_nickname="%s"'%nickname).proj('session_date', 'performance_easy').fetch(as_dict=True, order_by='session_date'))
-    rt = pd.DataFrame(((behavior_analysis.BehavioralSummaryByDate.ReactionTimeByDate * subject.Subject * subject.SubjectLab &
-       'subject_nickname="%s"'%nickname)).proj('session_date', 'median_reaction_time').fetch(as_dict=True, order_by='session_date'))
-    psych = pd.DataFrame(((behavior_analysis.BehavioralSummaryByDate.PsychResults * subject.Subject * subject.SubjectLab &
-       'subject_nickname="%s"'%nickname)).proj('session_date', 'n_trials_stim','threshold','bias','lapse_low','lapse_high').fetch(as_dict=True, order_by='session_date'))
-    
+    subj = subject.Subject * subject.SubjectLab & 'subject_nickname="%s"' % nickname
+    training = pd.DataFrame(behavior_analysis.SessionTrainingStatus * subject.Subject
+                            & 'subject_nickname="%s"' % nickname)
+    behav = pd.DataFrame((behavior_analysis.BehavioralSummaryByDate
+                          * subject.Subject * subject.SubjectLab
+                          & 'subject_nickname="%s"' % nickname).proj(
+                                  'session_date', 'performance_easy').fetch(
+                                          as_dict=True, order_by='session_date'))
+    rt = pd.DataFrame(((behavior_analysis.BehavioralSummaryByDate.ReactionTimeByDate
+                        * subject.Subject * subject.SubjectLab
+                        & 'subject_nickname="%s"' % nickname)).proj(
+                                'session_date', 'median_reaction_time').fetch(
+                                            as_dict=True, order_by='session_date'))
+    psych = pd.DataFrame(((behavior_analysis.BehavioralSummaryByDate.PsychResults
+                           * subject.Subject * subject.SubjectLab
+                           & 'subject_nickname="%s"' % nickname)).proj(
+                                    'session_date', 'n_trials_stim', 'threshold', 'bias',
+                                    'lapse_low', 'lapse_high').fetch(
+                                            as_dict=True, order_by='session_date'))
+
     if len(training) == 0:
-        print('No data found for subject %s'%nickname)
+        print('No data found for subject %s' % nickname)
         continue
-    
+
     # Find first session in which mouse is trained
-    if sum(training['training_status'] == 'trained') == 0 & sum(training['training_status'] == 'over40days') == 0:
-        learning.loc[i,'learned'] = 'in training'
-        learning.loc[i,'training_time'] = len(behav)
-    elif sum(training['training_status'] == 'trained') == 0 & sum(training['training_status'] == 'over40days') > 0:
-        learning.loc[i,'learned'] = 'over40days'
-        learning.loc[i,'training_time'] = len(behav)
+    if (sum(training['training_status'] == 'trained') == 0
+            & sum(training['training_status'] == 'over40days') == 0):
+        learning.loc[i, 'learned'] = 'in training'
+        learning.loc[i, 'training_time'] = len(behav)
+    elif (sum(training['training_status'] == 'trained') == 0
+          & sum(training['training_status'] == 'over40days') > 0):
+        learning.loc[i, 'learned'] = 'over40days'
+        learning.loc[i, 'training_time'] = len(behav)
     else:
-        first_trained_ind = min(training.loc[training['training_status'] == 'trained', 'session_start_time'].index.values)
-        first_day_ind = first_trained_ind - 1 #Get middle session of 3 day streak
+        first_trained_ind = min(training.loc[training['training_status'] == 'trained',
+                                             'session_start_time'].index.values)
+        first_day_ind = first_trained_ind - 1  # Get middle session of 3 day streak
         if training.loc[first_day_ind, 'training_status'] == 'wrong session type run':
             continue
         first_trained_session_datetime = training.loc[first_day_ind, 'session_start_time']
         first_trained_session_date = first_trained_session_datetime.date()
-        learning.loc[i,'learned'] = 'trained'
-        learning.loc[i,'date_learned'] = first_trained_session_date
-        learning.loc[i,'training_time'] = sum(behav.session_date < first_trained_session_date)
-        learning.loc[i,'perf_easy'] = float(behav.performance_easy[behav.session_date == first_trained_session_date])*100
+        learning.loc[i, 'learned'] = 'trained'
+        learning.loc[i, 'date_learned'] = first_trained_session_date
+        learning.loc[i, 'training_time'] = sum(behav.session_date < first_trained_session_date)
+        learning.loc[i, 'perf_easy'] = float(
+                behav.performance_easy[behav.session_date == first_trained_session_date])*100
         psych['n_trials'] = n_trials = [sum(s) for s in psych.n_trials_stim]
-        learning.loc[i,'n_trials'] = float(psych.n_trials[psych.session_date == first_trained_session_date])
-        learning.loc[i,'threshold'] = float(psych.threshold[psych.session_date == first_trained_session_date])
-        learning.loc[i,'bias'] = float(psych.bias[psych.session_date == first_trained_session_date])
-        learning.loc[i,'lapse_low'] = float(psych.lapse_low[psych.session_date == first_trained_session_date])
-        learning.loc[i,'lapse_high'] = float(psych.lapse_high[psych.session_date == first_trained_session_date])
+        learning.loc[i, 'n_trials'] = float(
+                psych.n_trials[psych.session_date == first_trained_session_date])
+        learning.loc[i, 'threshold'] = float(
+                psych.threshold[psych.session_date == first_trained_session_date])
+        learning.loc[i, 'bias'] = float(
+                psych.bias[psych.session_date == first_trained_session_date])
+        learning.loc[i, 'lapse_low'] = float(
+                psych.lapse_low[psych.session_date == first_trained_session_date])
+        learning.loc[i, 'lapse_high'] = float(
+                psych.lapse_high[psych.session_date == first_trained_session_date])
         if sum(rt.session_date == first_trained_session_date) == 0:
-            learning.loc[i,'reaction_time'] = float(rt.median_reaction_time[np.argmin(np.array(abs(rt.session_date - first_trained_session_date)))])*1000
+            learning.loc[i, 'reaction_time'] = float(
+                    rt.median_reaction_time[np.argmin(np.array(abs(
+                            rt.session_date - first_trained_session_date)))])*1000
         else:
-            learning.loc[i,'reaction_time'] = float(rt.median_reaction_time[rt.session_date == first_trained_session_date])*1000
-        
+            learning.loc[i, 'reaction_time'] = float(
+                    rt.median_reaction_time[rt.session_date == first_trained_session_date])*1000
+
     # Add mouse and lab info to dataframe
-    learning.loc[i,'mouse'] = nickname
+    learning.loc[i, 'mouse'] = nickname
     lab_name = subj.fetch1('lab_name')
-    learning.loc[i,'lab'] = lab_name
-    lab_time = reference.Lab * reference.LabLocation & 'lab_name="%s"'%lab_name
+    learning.loc[i, 'lab'] = lab_name
+    lab_time = reference.Lab * reference.LabLocation & 'lab_name="%s"' % lab_name
     time_zone = lab_time.fetch('time_zone')[0]
     if time_zone == ('Europe/Lisbon' or 'Europe/London'):
         time_zone_number = 0
@@ -112,19 +139,20 @@ for i, nickname in enumerate(subjects):
         time_zone_number = -5
     elif time_zone == 'America/Los_Angeles':
         time_zone_number = -7
-    learning.loc[i,'time_zone'] = time_zone_number
-    
+    learning.loc[i, 'time_zone'] = time_zone_number
+
 # Select mice that learned
-learned = learning[learning['learned'] == 'trained'] 
+learned = learning[learning['learned'] == 'trained']
 
 # Merge some labs
 pd.options.mode.chained_assignment = None  # deactivate warning
-learned.loc[learned['lab'] == 'zadorlab','lab'] = 'churchlandlab'
-learned.loc[learned['lab'] == 'hoferlab','lab'] = 'mrsicflogellab'
+learned.loc[learned['lab'] == 'zadorlab', 'lab'] = 'churchlandlab'
+learned.loc[learned['lab'] == 'hoferlab', 'lab'] = 'mrsicflogellab'
 
 # Add (n = x) to lab names
 for i in learned.index.values:
-    learned.loc[i,'lab_n'] = learned.loc[i,'lab'] + ' (n=' + str(sum(learned['lab'] == learned.loc[i,'lab'])) + ')'
+    learned.loc[i, 'lab_n'] = learned.loc[i, 'lab'] + ' (n=' + str(sum(
+            learned['lab'] == learned.loc[i, 'lab'])) + ')'
 
 # Initialize decoders
 print('\nDecoding of lab membership..')
@@ -132,40 +160,49 @@ decod = learned
 clf_rf = RandomForestClassifier(n_estimators=100)
 
 # Perform decoding of lab membership
-decoding_result = pd.DataFrame(columns=['original','original_shuffled','control','control_shuffled'])
+decoding_result = pd.DataFrame(columns=['original', 'original_shuffled',
+                                        'control', 'control_shuffled'])
 decoding_set = decod[decoding_metrics].values
 control_set = decod[decoding_metrics_control].values
 for i in range(iterations):
-    if np.mod(i+1,100) == 0:
-        print('Iteration %d of %d'%(i+1,iterations))        
+    if np.mod(i+1, 100) == 0:
+        print('Iteration %d of %d' % (i+1, iterations))
     # Original dataset
-    decoding_result.loc[i,'original'] = decoding(decoding_set, list(decod['lab']), clf_rf, num_splits)
-    decoding_result.loc[i,'original_shuffled'] = decoding(decoding_set, list(decod['lab'].sample(frac=1)), clf_rf, num_splits)
+    decoding_result.loc[i, 'original'] = decoding(decoding_set,
+                                                  list(decod['lab']), clf_rf, num_splits)
+    decoding_result.loc[i, 'original_shuffled'] = decoding(decoding_set,
+                                                           list(decod['lab'].sample(frac=1)),
+                                                           clf_rf, num_splits)
     # Positive control dataset
-    decoding_result.loc[i,'control'] = decoding(control_set, list(decod['lab']), clf_rf, num_splits)
-    decoding_result.loc[i,'control_shuffled'] = decoding(control_set, list(decod['lab'].sample(frac=1)), clf_rf, num_splits)
-   
+    decoding_result.loc[i, 'control'] = decoding(control_set,
+                                                 list(decod['lab']), clf_rf, num_splits)
+    decoding_result.loc[i, 'control_shuffled'] = decoding(control_set,
+                                                          list(decod['lab'].sample(frac=1)),
+                                                          clf_rf, num_splits)
 
-# Calculate if decoder performs above chance (positive values in perc indicate above chance-level performance)
-sig = np.percentile(decoding_result['original']-np.mean(decoding_result['original_shuffled']),5)
-sig_control = np.percentile(decoding_result['control']-np.mean(decoding_result['control_shuffled']),5)
+
+# Calculate if decoder performs above chance (positive values indicate above chance-level)
+sig = np.percentile(decoding_result['original']-np.mean(decoding_result['original_shuffled']), 5)
+sig_control = np.percentile(decoding_result['control']
+                            - np.mean(decoding_result['control_shuffled']), 5)
 
 # Plot decoding results
 seaborn_style()
-plt.figure(figsize=(4,5))
+plt.figure(figsize=(4, 5))
 fig = plt.gcf()
 ax1 = plt.gca()
-sns.violinplot(data=pd.concat([decoding_result['original']-decoding_result['original_shuffled'], 
-                               decoding_result['control']-decoding_result['control_shuffled']], axis=1), color=[0.6,0.6,0.6], ax=ax1)
-ax1.plot([-1,5],[0,0],'r--')
-ax1.set(ylabel='Decoding performance over chance level\n(F1 score)', title='Random forest classifier', 
-        ylim=[-0.4,0.8], xlim=[-0.8,1.8], xticklabels=['Decoding of\nlab membership', 'Positive\ncontrol'])
+sns.violinplot(data=pd.concat([decoding_result['original']-decoding_result['original_shuffled'],
+                              decoding_result['control']-decoding_result['control_shuffled']],
+                              axis=1), color=[0.6, 0.6, 0.6], ax=ax1)
+ax1.plot([-1, 5], [0, 0], 'r--')
+ax1.set(ylabel='Decoding performance over chance level\n(F1 score)',
+        title='Random forest classifier',
+        ylim=[-0.4, 0.8], xlim=[-0.8, 1.8],
+        xticklabels=['Decoding of\nlab membership', 'Positive\ncontrol'])
 ax1.text(0, 0.68, 'n.s.', fontsize=12, ha='center')
 ax1.text(1, 0.68, '***', fontsize=15, ha='center', va='center')
 plt.setp(ax1.xaxis.get_majorticklabels(), rotation=60)
 
-plt.tight_layout(pad = 2)
-plt.savefig(join(path,'figure6_panel_decoding.pdf'), dpi=300)
-plt.savefig(join(path,'figure6_panel_decoding.png'), dpi=300)
-
-
+plt.tight_layout(pad=2)
+plt.savefig(join(path, 'figure6_panel_decoding.pdf'), dpi=300)
+plt.savefig(join(path, 'figure6_panel_decoding.png'), dpi=300)
