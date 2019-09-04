adb68af462e16c6a8e55bb447c3bc746ea6fd34c
Gaelle <gaelle.chapuis.ibl@gmail.com>
2019-09-04 19:50:08 -0400

    cleanify

--- tutorial/gc__script.py
+++ tutorial/gc__script.py
@@ -1,5 +1,7 @@
 from tutorial import get_specific_mice
 # %%
-mice_m = get_specific_mice.get_trained_mice(sex='m', project_name='ibl_neuropixel_brainwide_01')
-mice_f = get_specific_mice.get_trained_mice(sex='f', project_name='ibl_neuropixel_brainwide_01')
+mice_m = get_specific_mice.get_mice(sex='m', project_name='ibl_neuropixel_brainwide_01',
+                                    training_status='trained')
+mice_f = get_specific_mice.get_mice(sex='f', project_name='ibl_neuropixel_brainwide_01',
+                                    training_status='trained')
 # %%
