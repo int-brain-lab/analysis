bd3bb7646e4c75b46715f14137e83d095e57ec05
Gaelle <gaelle.chapuis.ibl@gmail.com>
2019-09-04 19:11:23 -0400

    added __init__ so can run module

--- tutorial/gc__script.py
+++ tutorial/gc__script.py
@@ -1,4 +1,4 @@
-import get_specific_mice.py
-
+from tutorial import get_specific_mice
+# %%
 mice_m = get_specific_mice.get_trained_mice(sex='m')
-mice_f = get_specific_mice.get_trained_mice(sex='m')
+mice_f = get_specific_mice.get_trained_mice(sex='f')
--- tutorial/get_specific_mice.py
+++ tutorial/get_specific_mice.py
@@ -2,20 +2,25 @@
 # find mice in database, that are: 1) trained, 2) male or female, 3) belonging to specific lab
 # Note: for a quick an dirty way to get a selection of mice, you can e.g. run:
 # attest2 = subject.Subject * subject.SubjectProject * subject.SubjectLab
-# select_mice = attest2 & 'sex="M"' & 'subject_project="ibl_neuropixel_brainwide_01"' & 'lab_name="mrsicflogellab"'
+# select_mice = attest2 & 'sex="M"' & 'subject_project="ibl_neuropixel_brainwide_01"' &
+# 'lab_name="mrsicflogellab"'
 
 # %%
-import datajoint as dj
-from ibl_pipeline import reference, subject, action, acquisition, data, behavior, ephys
+# import datajoint as dj
+# from ibl_pipeline import reference, subject, action, acquisition, data, behavior, ephys
+from ibl_pipeline import subject
 from ibl_pipeline.analyses import behavior as behavior_analyses
 # %%
 SEX_DEFAULTS = ('m', 'M', 'f', 'F', 'mf')
 
 
-def get_trained_mice(sex='mf', lab_name=None, training_status='trained', format='frame'):
+def get_trained_mice(sex='mf', lab_name=None, training_status='trained', format='frame',
+                     project_name=None):
 
     if not isinstance(training_status, str):
         raise ValueError('training_status has to be a string')
+    if not isinstance(project_name, str):
+        raise ValueError('project_name has to be a string')
     if sex not in SEX_DEFAULTS:
         raise ValueError('if a specific sex is wanted, sex has to be written either m or f.')
 
@@ -31,11 +36,17 @@ def get_trained_mice(sex='mf', lab_name=None, training_status='trained', format=
     else:
         subject_lab = subject.SubjectLab & {'lab_name': lab_name}
 
+    # Project criterion
+    if project_name is None:
+        project = subject.SubjectProject
+    else:
+        project = subject.SubjectProject & (f'subject_project="{project_name}"')
+
     #  Training criterion
     trained = behavior_analyses.SessionTrainingStatus & (f'training_status="{training_status}"')
 
     # Assemble multiple conditions
-    subject_output = subject_sex & trained & subject_lab
+    subject_output = subject_sex & trained & subject_lab & project
 
     #  Fetch data
     return subject_output.fetch(format=format)
