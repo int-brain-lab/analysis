ffcf1ff453ccecc5b14cc9abe0600c266d0c6b57
Anne Urai <anne.urai@gmail.com>
2019-11-09 18:09:49 -0500

    biasshift by blocks as a function of task knowledge

--- python/human_biasedChoiceWorld_summary.py
+++ python/human_biasedChoiceWorld_summary.py
@@ -87,16 +87,82 @@ fig.map(plot_chronometric, "signed_contrast", "rt", "subject_nickname").add_lege
 fig.set_axis_labels('Signed contrast (%)', 'RT (s)')
 fig.savefig(os.path.join(figpath, "human_sfn_chronfuncs_biased.png"), dpi=300)
 
+
+# ================================= #
+# BIASSHIFT AS A FUNCTION OF TASK KNOWLEDGE
+# ================================= #
+
+# BY BIASED BLOCKS
+fig = sns.FacetGrid(behav, col='subject_nickname', col_wrap=12,
+                    hue="probabilityLeft",
+                    palette=sns.diverging_palette(20, 220, n=2, center="dark"))
+fig.map(plot_psychometric, "signed_contrast", "choice_right", "subject_nickname").add_legend()
+fig.set_axis_labels('Signed contrast (%)', 'Rightward choice (%)')
+fig.savefig(os.path.join(figpath, "human_sfn_psychfuncs_biased_allsj.png"), dpi=200)
+
+# compute the bias shift between blocks for each person
+# use psychometric fit
+subj_query = (sfn_subject.Subject & 'subject_nickname LIKE "human%"') \
+             * (acquisition.Session.proj(session_date='date(session_start_time)'))
+subj_data = (subj_query * behavioral_analyses.PsychResultsBlock).fetch(format='frame').reset_index()
+subj_data.sex.fillna(value='U', inplace=True)
+
+subj_data2 = pd.pivot_table(subj_data, index=['subject_nickname', 'task_knowledge'],
+                            columns='prob_left_block', values='bias').reset_index()
+subj_data2['biasshift'] = subj_data2[80] - subj_data2[20]
+
+fig, ax = plt.subplots(1, 1, figsize=[3.5, 3.5])
+sns.boxplot(x="task_knowledge", y="biasshift",
+                   palette=sns.color_palette("cubehelix", 4), data=subj_data2, ax=ax)
+sns.swarmplot(x="task_knowledge", y="biasshift",
+                   palette=sns.color_palette("cubehelix", 4), data=subj_data2, ax=ax)
+plt.tight_layout()
+fig.savefig(os.path.join(figpath, "human_sfn_biasshift.pdf"))
+
+# compute the bias shift between blocks for each person
+# use choice fraction
+behav_zero = behav.loc[behav.signed_contrast == 0, :]
+repeat = behav_zero.groupby(['subject_nickname', 'probabilityLeft', 'task_knowledge'])['choice'].mean().reset_index()
+repeat2 = pd.pivot_table(repeat, index=['subject_nickname', 'task_knowledge'],
+                            columns='probabilityLeft', values='choice').reset_index()
+repeat2['biasshift'] = repeat2[20] - repeat2[80]
+
+fig, ax = plt.subplots(1, 1, figsize=[3.5, 3.5])
+sns.boxplot(x="task_knowledge", y="biasshift", boxprops={'facecolor':'None'},
+                   palette=sns.color_palette("cubehelix", 4), data=repeat2, ax=ax)
+sns.swarmplot(x="task_knowledge", y="biasshift",
+                   palette=sns.color_palette("cubehelix", 4), data=repeat2, ax=ax)
+ax.set(xlabel='Task knowledge', ylabel='$\Delta$ Rightward choice (%)')
+plt.tight_layout()
+fig.savefig(os.path.join(figpath, "human_sfn_biasshift_choicefraction.png"), dpi=300)
+
 # ================================= #
 # LEARNING CURVES
 # ================================= #
 
-fig = sns.FacetGrid(behav, hue="task_knowledge", hue_order=[1,2,3,4],
+# behav.correct.fillna(value=0., inplace=True)
+
+rolling_dat = behav.groupby(['task_knowledge',
+                             'subject_nickname'])[['trial',
+                                                   'correct']].rolling(20).mean().reset_index()
+fig = sns.FacetGrid(rolling_dat,
+                    hue="task_knowledge", hue_order=[1,2,3,4],
                     palette=sns.color_palette("cubehelix", 4))
-fig.map(sns.lineplot, "trial_id", "correct")
+fig.map(sns.lineplot, "trial", "correct", ci=None).add_legend()
 fig.savefig(os.path.join(figpath, "human_sfn_learning_taskknowledge.pdf"))
 
+shell()
+rolling_dat = behav.groupby(['task_knowledge'])[['trial', 'rt']].rolling(10).median()
+fig = sns.FacetGrid(rolling_dat,
+                    hue="task_knowledge", hue_order=[1,2,3,4],
+                    palette=sns.color_palette("cubehelix", 4))
+fig.map(sns.lineplot, "trial_id", "rt").add_legend()
+fig.savefig(os.path.join(figpath, "human_sfn_learningspeed_taskknowledge.pdf"))
+
+# ================================= #
 # plot one curve for each person
+# ================================= #
+
 fig = sns.FacetGrid(behav,
                     col="subject_nickname", col_wrap=12, sharex=True, sharey=True,
                     hue="task_knowledge")
