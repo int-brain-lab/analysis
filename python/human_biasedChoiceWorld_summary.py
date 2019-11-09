f91fe5fed885aca0371206110211d7fc24e8540c
Anne Urai <anne.urai@gmail.com>
2019-11-09 12:09:16 -0500

    overview of demographic and behavior data from SfN

--- python/human_biasedChoiceWorld_summary.py
+++ python/human_biasedChoiceWorld_summary.py
@@ -8,44 +8,100 @@ import numpy as np
 import sys, os, time
 import matplotlib.pyplot as plt
 import seaborn as sns
+sns.set(style="ticks", context="paper", font_scale=1.4)
+
 import datajoint as dj
+from dj_tools import *
 
 # import wrappers etc
 from ibl_pipeline import reference, subject, action, acquisition, data, behavior
 from ibl_pipeline.analyses import behavior as behavioral_analyses
-figpath  = os.path.join(os.path.expanduser('~'), 'Data/Figures_IBL')
+sfn_subject = dj.create_virtual_module('subject', 'group_shared_sfndata')
+figpath = os.path.join(os.path.expanduser('~'), 'Data/Figures_IBL')
 
 # ================================= #
-# GRAB ALL DATA FROM DATAJOINT
+# SUMMARY OF DEMOGRAPHICS
 # ================================= #
 
-subj_query = (subject.Subject & 'subject_nickname LIKE "human%"') \
+subj_query = (sfn_subject.Subject & 'subject_nickname LIKE "human%"') \
              * (acquisition.Session.proj(session_date='date(session_start_time)'))
-behav_all = (subj_query * behavior.TrialSet.Trial).fetch(format='frame').reset_index()
-assert(not behav_all.empty)
+subj_data = (subj_query * behavior.TrialSet).fetch(format='frame').reset_index()
+subj_data.sex.fillna(value='U', inplace=True)
 
-print(behav_all.describe())
-print(behav_all.subject_nickname.unique())
+# compute some simple things
+subj_data['performance'] = subj_data.n_correct_trials / subj_data.n_trials
+subj_data['trial_duration'] = subj_data.trials_end_time / subj_data.n_trials
+subj_data.fillna(value=pd.np.nan, inplace=True)
+
+g = sns.pairplot(subj_data, vars=['age', 'task_knowledge', 'aq_score', 'performance', 'n_trials'],
+                 hue='sex', diag_kind='hist', diag_kws={'histtype':'step'})
+g.savefig(os.path.join(figpath, "human_sfn_overview.png"), dpi=300)
+plt.close('all')
 
 # ================================= #
 # GRAB ALL DATA FROM DATAJOINT
 # ================================= #
-#
-# for sidx, behav in behav_all.groupby(['session_start_time']):
-#
-#     print(sidx)
-#
-#     # 1. recode timings
-#     session_duration = (behav.trial_end_time.iloc[-1] - behav.trial_start_time.iloc[0])
-#     avg_trial_duration = session_duration / len(behav.trial_end_time)
-#     print("Total duration %f minutes, %d trials, s/trials %f"%(session_duration / 60,
-#                                                                len(behav.trial_end_time),
-#                                                                avg_trial_duration))
-#
-#     # what is this made up of?
-#     behav['time_prestim'] = behav.trial_go_cue_trigger_time - behav.trial_start_time
-#     behav['time_rt'] = behav.trial_response_time - behav.trial_go_cue_trigger_time
-#     behav['time_postrep'] = behav.trial_end_time - behav.trial_response_time
-#
-#     print(behav[['time_prestim', 'time_rt', 'time_postrep', 'trial_feedback_type']].groupby([
-#     'trial_feedback_type']).max())
+
+subj_query = (sfn_subject.Subject & 'subject_nickname LIKE "human%"') \
+             * (acquisition.Session.proj(session_date='date(session_start_time)'))
+behav = (subj_query * behavior.TrialSet.Trial).fetch(format='frame').reset_index()
+assert(not behav.empty)
+behav = dj2pandas(behav)
+
+print(behav.describe())
+print(behav.subject_nickname.unique())
+
+# ================================= #
+# PLOT
+# ================================= #
+
+#PSYCHOMETRIC
+fig = sns.FacetGrid(behav, hue="task_knowledge", hue_order=[1,2,3,4],
+                    palette=sns.color_palette("cubehelix", 4))
+fig.map(plot_psychometric, "signed_contrast", "choice_right", "subject_nickname").add_legend()
+fig.set_axis_labels('Signed contrast (%)', 'Rightward choice (%)')
+fig.savefig(os.path.join(figpath, "human_sfn_psychfuncs_taskknowledge.png"), dpi=300)
+
+# CHRONOMETRIC
+fig = sns.FacetGrid(behav,
+                    hue="task_knowledge", hue_order=[1,2,3,4],
+                    palette=sns.color_palette("cubehelix", 4))
+fig.map(plot_chronometric, "signed_contrast", "rt", "subject_nickname").add_legend()
+fig.set_axis_labels('Signed contrast (%)', 'Trial duration (s)')
+fig.savefig(os.path.join(figpath, "human_sfn_chronfuncs_taskknowledge.png"), dpi=300)
+
+
+# BY BIASED BLOCKS
+fig = sns.FacetGrid(behav,
+                    hue="probabilityLeft",
+                    palette=sns.diverging_palette(20, 220, n=2, center="dark"))
+fig.map(plot_psychometric, "signed_contrast", "choice_right", "subject_nickname").add_legend()
+fig.set_axis_labels('Signed contrast (%)', 'Rightward choice (%)')
+fig.savefig(os.path.join(figpath, "human_sfn_psychfuncs_biased.png"), dpi=300)
+
+# BY BIASED BLOCKS
+fig = sns.FacetGrid(behav,
+                    hue="probabilityLeft",
+                    palette=sns.diverging_palette(20, 220, n=2, center="dark"))
+fig.map(plot_chronometric, "signed_contrast", "rt", "subject_nickname").add_legend()
+fig.set_axis_labels('Signed contrast (%)', 'RT (s)')
+fig.savefig(os.path.join(figpath, "human_sfn_chronfuncs_biased.png"), dpi=300)
+
+# ================================= #
+# LEARNING CURVES
+# ================================= #
+
+fig = sns.FacetGrid(behav, hue="task_knowledge", hue_order=[1,2,3,4],
+                    palette=sns.color_palette("cubehelix", 4))
+fig.map(sns.lineplot, "trial_id", "correct")
+fig.savefig(os.path.join(figpath, "human_sfn_learning_taskknowledge.pdf"))
+
+# plot one curve for each person
+fig = sns.FacetGrid(behav,
+                    col="subject_nickname", col_wrap=12, sharex=True, sharey=True,
+                    hue="task_knowledge")
+fig.map(plot_psychometric, "signed_contrast", "choice_right",
+        "subject_nickname")
+fig.set_titles("{col_name}")
+fig.set_axis_labels('Signed contrast (%)', 'Trial duration (s)')
+fig.savefig(os.path.join(figpath, "human_sfn_psychfuncs_allsj.pdf"))
