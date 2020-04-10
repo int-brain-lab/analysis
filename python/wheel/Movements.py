86ff7e2ed851c6e13232d273f720aee1b043bb63
Miles Wells <k1o0@3tk.co>
2020-04-10 12:59:43 +0300

    Changes to table and rt calculation

--- python/wheel/Movements.py
+++ python/wheel/Movements.py
@@ -26,8 +26,8 @@ class WheelMoveSet(dj.Imported):
     -> acquisition.Session
     ---
     n_movements:            int # total number of movements within the session
-    total_displacement:     float # total displacement of the wheel during session 
-    total_distance:         float # total movement of the wheel
+    total_displacement:     float # total displacement of the wheel during session in radians
+    total_distance:         float # total movement of the wheel in radians
     """
 
     class Move(dj.Part):
@@ -38,8 +38,8 @@ class WheelMoveSet(dj.Imported):
         ---
         movement_onset:         float # time of movement onset in seconds from session start
         movement_offset:        float # time of movement offset in seconds from session start 
-        max_velocity:           float # peak velocity of movement
-        movement_amplitude:     float # movement amplitude
+        max_velocity:           float # time of movement's peak velocity
+        movement_amplitude:     float # the absolute peak amplitude relative to onset position
         """
 
     key_source = behavior.CompleteWheelSession
@@ -55,7 +55,7 @@ class WheelMoveSet(dj.Imported):
         logger.info('WheelMoves for session %s, %s', str(eid), ver)
 
         try:  # Should be able to remove this
-            wheel = one.load_object(str(eid), 'wheel')  # Fails for some sessions, e.g. CSHL_007\2019-11-08\002
+            wheel = one.load_object(str(eid), 'wheel')
             all_loaded = \
                 all([isinstance(wheel[lab], np.ndarray) for lab in wheel]) and \
                 all(k in wheel for k in ('timestamps', 'position'))
@@ -90,6 +90,9 @@ class WheelMoveSet(dj.Imported):
         except AssertionError as ex:
             logger.exception(str(ex))
             raise
+        except Exception as ex:
+            logger.exception(str(ex))
+            raise
 
         try:
             # Convert the pos threshold defaults from samples to correct unit
@@ -114,6 +117,11 @@ class WheelMoveSet(dj.Imported):
         key['n_movements'] = on.size  # total number of movements within the session
         key['total_displacement'] = float(np.diff(pos[[0, -1]]))  # total displacement of the wheel during session
         key['total_distance'] = float(np.abs(np.diff(pos)).sum())  # total movement of the wheel
+        if units is 'cm':  # convert to radians
+            key['total_displacement'] = wh.cm_to_rad(key['total_displacement'])
+            key['total_distance'] = wh.cm_to_rad(key['total_distance'])
+            amp = wh.cm_to_rad(amp)
+
         self.insert1(key)
 
         keys = ('move_id', 'movement_onset', 'movement_offset', 'max_velocity', 'movement_amplitude')
@@ -126,25 +134,28 @@ class WheelMoveSet(dj.Imported):
 @schema
 class MovementTimes(dj.Computed):
     definition = """
-    # Wheel movements occurring within a session
+    # Trial movements table
     -> behavior.TrialSet.Trial
     ---
     -> WheelMoveSet.Move
-    reaction_time:          float # time in seconds from go cue to last movement onset of the trial
-    movement_time:          float # time in seconds from last movement onset to feedback time
+    reaction_time:          float # time in seconds from go cue to first sufficiently large movement onset of the trial
+    final_movement:         bool # indicates whether movement onset was the one that reached threshold
+    movement_time:          float # time in seconds from first movement onset to feedback time
     response_time:          float # time in seconds from go cue to feedback time 
-    movement_onset:         float # time in seconds when last movement onset occurred
+    movement_onset:         float # time in seconds when first movement onset occurred
     """
 
     key_source = behavior.CompleteTrialSession & WheelMoveSet
 
     def make(self, key):
+        THRESH = .1  # peak amp should be at least .1 rad; ~1/3rd of the threshold
         eid, ver = (acquisition.Session & key).fetch1('session_uuid', 'task_protocol')  # For logging purposes
         logger.info('MovementTimes for session %s, %s', str(eid), ver)
         query = (WheelMoveSet.Move & key).proj(
             'move_id',
             'movement_onset',
-            'movement_offset')
+            'movement_offset',
+            'movement_amplitude')
         wheel_move_data = query.fetch(order_by='move_id')
 
         query = (behavior.TrialSet.Trial & key).proj(
@@ -161,21 +172,20 @@ class MovementTimes(dj.Computed):
             return
 
         all_move_onsets = wheel_move_data['movement_onset']
+        peak_amp = wheel_move_data['movement_amplitude']
+        flinch = peak_amp < THRESH
         go_trial = trial_data['trial_response_choice'] != 'No Go'
-        feedback_times = trial_data['trial_feedback_time'][go_trial]
-        start_times = trial_data['trial_start_time'][go_trial]
-        cue_times = trial_data['trial_go_cue_time'][go_trial]
+        feedback_times = trial_data['trial_feedback_time']
+        cue_times = trial_data['trial_go_cue_time']
 
         # Check integrity of feedback and start times
         try:
-            # Assert no nans and monotonically increasing for start times
-            assert np.all(~np.isnan(start_times)) and np.all(np.diff(start_times)), 'start times missing or disordered'
             # Log presence of nans in feedback times (common)
             nan_trial = np.isnan(feedback_times)
             if nan_trial.any():
                 n_feedback_nans = np.count_nonzero(nan_trial)
                 logger.warning('%i feedback_times nan', np.count_nonzero(nan_trial))
-                response_times = trial_data['trial_response_time'][go_trial]
+                response_times = trial_data['trial_response_time']
                 if n_feedback_nans > np.count_nonzero(np.isnan(response_times)):
                     logger.warning('using response times instead of feedback times')
                     feedback_times = response_times
@@ -188,7 +198,7 @@ class MovementTimes(dj.Computed):
                 # If all nan, use stim on
                 if np.isnan(cue_times).all():
                     logger.warning('trial_go_cue_time is all nan, using trial_stim_on_time')
-                    cue_times = trial_data['trial_stim_on_time'][go_trial]
+                    cue_times = trial_data['trial_stim_on_time']
                     if np.isnan(cue_times).any():
                         n_nan = 'all' if np.isnan(cue_times).all() else str(np.count_nonzero(np.isnan(cue_times)))
                         logger.warning('trial_stim_on_time nan for %s trials', n_nan)
@@ -197,52 +207,61 @@ class MovementTimes(dj.Computed):
             # Assert all cue times are montonically increasing
             assert np.all(np.diff(cue_times[~np.isnan(cue_times)]) > 0), 'cue times not monotonically increasing'
             # Assert all start times occur before feedback times
-            assert np.all((feedback_times[~nan_trial] - start_times) > 0), 'feedback occurs before start time'
+            # assert np.all((feedback_times[~nan_trial] - start_times) > 0), 'feedback occurs before start time'
         except AssertionError as ex:
             logger.exception('Movement integrity check failed: ' + str(ex))
             raise
 
-        # Segregate move onsets by trial
-        onsets_by_trial = ()
-        for t1, t2 in zip(start_times, feedback_times):
-            if ~np.isnan(t2):
-                mask = (all_move_onsets > t1) & (all_move_onsets < t2)
-                if np.any(mask):
-                    onsets_by_trial += np.where(mask)
-
-        # Fetch last movement of each trial (end value of each array)
-        ids = np.array([trial.take(-1) for trial in onsets_by_trial])  # Identical
-        #  ids = np.array(list(zip(*onsets_by_trial))[-1])
-
+        # Get minimum quiescent period for session
         try:
-            # Test movements for all go trials
-            assert len(ids) == np.count_nonzero(go_trial)
-            onsets = all_move_onsets[ids]  # Insert onsets for each trial
-            assert np.all((onsets - cue_times) > 0), 'Not all response times positive'
-        except AssertionError:
-            logger.exception('failed to find onsets for all trials')
-            raise
+            one = ONE()
+            task_params = one.load_object(str(eid), '_iblrig_taskSettings.raw')
+            min_qt = task_params['raw']['QUIESCENT_PERIOD']
+            assert min_qt
+        except BaseException:
+            logger.warning('failed to load min quiescent time')
+            min_qt = 0.2
 
-        movement_data = zip(
-            trial_data['trial_id'][go_trial],  # trial_id
+        # Find first significant movement for each trial.  To be counted, the movement must
+        # occur between go cue / stim on and before feedback / response time.  The movement
+        # onset is sometimes just before the cue (occurring in the gap between quiescence end and
+        # cue start, or during the quiescence period but sub-threshold).  The movement is
+        # sufficiently large if it is greater than or equal to THRESH
+
+        # Initialize as nans
+        onsets = np.full(trial_data['trial_id'].shape, np.nan)
+        ids = np.full(trial_data['trial_id'].shape, int(-1))
+        final_movement = np.zeros(trial_data['trial_id'].shape, bool)
+        # Iterate over trials, extracting onsets approx. within closed-loop period
+        for i, (t1, t2) in enumerate(zip(cue_times - min_qt, feedback_times)):
+            if ~np.isnan(t2 - t1):  # If both timestamps defined
+                mask = (all_move_onsets > t1) & (all_move_onsets < t2)
+                if np.any(mask):  # If any onsets for this trial
+                    trial_onset_ids, = np.where(mask)
+                    if np.any(~flinch[mask]):  # If any trial moves were sufficiently large
+                        ids[i] = trial_onset_ids[~flinch[mask]][0]  # Find first large move id
+                        onsets[i] = all_move_onsets[ids[i]]  # Save first large move onset
+                        final_movement[i] = ids[i] == trial_onset_ids[-1]  # Final move of trial
+                else:  # Check if trial was no-go
+                    if ~go_trial[i]:  # Report if not no-go
+                        logger.warning('failed to find any onsets for trial id %i', i+1)
+            else:  # Log missing timestamps
+                logger.warning('no reliable times for trial id %i', i + 1)
+
+        # Create matrix of values for insertion into table
+        movement_data = np.c_[
+            trial_data['trial_id'],  # trial_id
             ids,  # wheel_move_id
-            onsets,  # movement_onset
-            feedback_times - cue_times,  # response_time
             onsets - cue_times,  # reaction_time
-            feedback_times - onsets  # movement_time
-        )
+            final_movement,  # final_movement
+            feedback_times - onsets,  # movement_time
+            feedback_times - cue_times,  # response_time
+            onsets  # movement_onset
+        ]
         data = []
-        for n, mid, onset, resp, rt, mvmt in movement_data:
-            if np.isnan([n, mid, onset, resp, rt, mvmt]).any():
-                logger.warning('nan found for trial %i', n)
-                continue
-            key.update({
-                'move_id': mid,
-                'trial_id': n,
-                'movement_onset': onset,
-                'response_time': resp,
-                'reaction_time': rt,
-                'movement_time': mvmt})
-            data.append(key.copy())
-            # self.insert1(key)
+        for row in movement_data:
+            if np.isnan(row).any():  # don't insert; leave as null
+                logger.warning('nan found for trial %i', row[0])
+            else:  # insert row
+                data.append(tuple([*key.values(), *list(row)]))
         self.insert(data)
