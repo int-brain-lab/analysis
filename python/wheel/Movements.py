10e06ff29b8f4dd8f198c5df9a88f05be15ef8ed
Miles Wells <k1o0@3tk.co>
2020-10-05 15:37:43 +0300

    Refector of MovementTimes

--- python/wheel/Movements.py
+++ python/wheel/Movements.py
@@ -9,7 +9,6 @@ from oneibl.one import ONE
 import brainbox.behavior.wheel as wh
 from ibllib.io.extractors.training_wheel import extract_wheel_moves, extract_first_movement_times, infer_wheel_units
 from ibllib.io.params import getfile
-import alf.io
 
 
 logger = logging.getLogger(__name__)
@@ -69,7 +68,10 @@ class WheelMoveSet(dj.Imported):
                 all([isinstance(wheel[lab], np.ndarray) for lab in wheel]) and \
                 all(k in wheel for k in ('timestamps', 'position'))
             assert all_loaded, 'wheel data missing'
-            alf.io.check_dimensions(wheel)
+
+            # If times and timestamps present, drop times
+            if {'times', 'timestamps'}.issubset(wheel):
+                wheel.pop('times')
             wheel_moves = extract_wheel_moves(wheel.timestamps, wheel.position)
         except ValueError:
             logger.exception('Failed to find movements')
@@ -148,24 +150,34 @@ class MovementTimes(dj.Computed):
         eid, ver = (acquisition.Session & key).fetch1('session_uuid', 'task_protocol')
         logger.info('MovementTimes for session %s, %s', str(eid), ver)
 
-        # Get required data from wheel moves and trials tables
-        query = (WheelMoveSet.Move & key).proj(
-            'move_id',
-            'movement_onset',
-            'movement_offset',
-            'movement_amplitude')
-        wheel_move_data = query.fetch(order_by='move_id')
+        # Get required data from wheel moves and trials tables, each as a dict of numpy arrays
+        fields = ('move_id', 'movement_onset', 'movement_offset', 'movement_amplitude')
+        wheel_move_data = {k: v.values for k, v in (
+            (
+                (WheelMoveSet.Move & key)
+                .proj(*fields)
+                .fetch(order_by='move_id', format='frame')
+                .reset_index()
+                .drop(['subject_uuid', 'session_start_time'], axis=1)
+                .rename(columns={'movement_amplitude': 'peakAmplitude'})
+                .iteritems()
+            )
+        )}
 
-        query = (behavior.TrialSet.Trial & key).proj(
-            'trial_response_choice',
-            'trial_response_time',
-            'trial_stim_on_time',
-            'trial_go_cue_time',
-            'trial_feedback_time',
-            'trial_start_time')
-        trial_data = query.fetch(order_by='trial_id')
+        fields = ('trial_response_choice', 'trial_response_time', 'trial_stim_on_time',
+                  'trial_go_cue_time', 'trial_feedback_time', 'trial_start_time')
+        trial_data = {k: v.values for k, v in (
+            (
+                (behavior.TrialSet.Trial & key)
+                .proj(*fields)
+                .fetch(order_by='trial_id', format='frame')
+                .reset_index()
+                .drop(['subject_uuid', 'session_start_time'], axis=1)
+                .iteritems()
+            )
+        )}
 
-        if trial_data.size == 0 or wheel_move_data.size == 0:
+        if trial_data['trial_id'].size == 0 or wheel_move_data['move_id'].size == 0:
             logger.warning('Missing DJ trial or move data')
             return
 
@@ -182,7 +194,7 @@ class MovementTimes(dj.Computed):
         # closed-loop periods by taking the minimum of go_cue and stim_on, response and feedback.
         start = np.nanmin(np.c_[trial_data['trial_stim_on_time'], trial_data['trial_go_cue_time']], axis=1)
         end = np.nanmin(np.c_[trial_data['trial_response_time'], trial_data['trial_feedback_time']], axis=1)
-        assert np.all(start > end) and np.all(np.diff(start) > 0), 'timestamps not increasing'
+        assert np.all(start < end) and np.all(np.diff(start) > 0), 'timestamps not increasing'
         go_trial = trial_data['trial_response_choice'] != 'No Go'
 
         # Check we have times for at least some trials
@@ -190,7 +202,6 @@ class MovementTimes(dj.Computed):
         assert ~nan_trial.all(), 'no reliable trials times for session'
 
         # Rename data for the firstMovement_times extractor function
-        wheel_move_data['peakAmplitude_times'] = wheel_move_data.pop('movement_amplitude')
         wheel_move_data['intervals'] = np.c_[
             wheel_move_data['movement_onset'], wheel_move_data['movement_offset']
         ]
@@ -202,16 +213,18 @@ class MovementTimes(dj.Computed):
         # cue start, or during the quiescence period but sub-threshold).  The movement is
         # sufficiently large if it is greater than or equal to THRESH
         onsets, final_movement, ids = extract_first_movement_times(wheel_move_data, trial_data, min_qt)
+        move_ids = np.full_like(onsets, np.nan)
+        move_ids[~np.isnan(onsets)] = ids
 
         # Check if any movements failed to be detected
         n_nan = np.count_nonzero(np.isnan(onsets[go_trial]))
-        if n_nan.any():
-            logger.warning('failed to detect movement on i% go trials', n_nan)
+        if n_nan > 0:
+            logger.warning('failed to detect movement on %i go trials', n_nan)
 
         # Create matrix of values for insertion into table
         movement_data = np.c_[
             trial_data['trial_id'],  # trial_id
-            ids,  # wheel_move_id
+            move_ids,  # wheel_move_id
             onsets - start,  # reaction_time
             final_movement,  # final_movement
             end - onsets,  # movement_time
